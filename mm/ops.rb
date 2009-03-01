1bde2d4b03417f7623dbf0117cdc874bc60fa890
Dave Parfitt <diparfitt@gmail.com>
2009-03-01 09:49:19 -0500

    Initial import of code

---
+++ mm/ops.rb
@@ -0,0 +1,236 @@
+#
+#    MetaMatter flow based scripting system
+#    Copyright (C) 2009 Dave Parfitt
+# 
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+# 
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+# 
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+module MM
+###############################################################################
+#
+# OPS
+#
+###############################################################################
+# need to iterate over these and try to come up with a small set that makes sense!
+
+
+# might want to rethink this class
+class StartupEcho < Op
+  input :echoin, :echo
+  output :echoout
+  config :message
+
+  def initialize
+	super
+ 	@message = "Foo"	
+  end
+
+  def starting
+    echoout.out(@message)
+  end
+
+  def echo(value)
+    echoout.out(value)
+  end
+end
+
+class Print < Op
+  input :printin, :print
+  output :printout 
+  config :fd
+  config :prefix # couldn't resist even though
+  				 # there should really be an op like "prepend"
+ 
+  def initialize
+      super
+      @prefix = ""   
+	  @fd = STDOUT
+  end
+ 
+  def print(value)
+    @fd.puts "#{prefix}#{value}"   
+    printout.out(value)
+  end
+
+  def stopping
+	# hmmmm ... what to do about closing FDs?
+	#if @fd != STDOUT then
+  	  #@fd.close
+	#end
+  end
+end
+
+
+class Gate < Op
+  input :gatein, :valuein
+  output :gateout
+
+  def initialize
+    super()
+    @queues = {}   
+    @queueorder = []
+    @windowsize = 0
+  end
+
+  def valuein(value)
+    #nop-> it's all done in receivedpacket
+  end
+
+  def receivedpacket(packet)
+    if packet.destinput == "valuein"    
+      #puts "Gate received a packet from #{packet.sourceoutput}"
+      @queues[packet.sourceoutput].push packet.value
+      while windowfull
+        gatefull
+      end
+    end
+  end
+
+  def windowfull
+    count = 0
+    @queues.each_pair do |conn,q|
+      if q.size > 0
+        count += 1
+      end
+    end
+    if count == @windowsize
+      true
+    else
+      false
+    end
+  end
+
+  def gatefull
+    values = []
+    @queueorder.each do |key|   
+      list = @queues[key]
+      values << list.shift
+    end
+    #puts "Gate is releasing a packet: #{values}"
+    gateout.out values   
+  end
+
+  def connectedto(output)   
+    @queueorder << output
+    @queues[output] = Array.new
+    @windowsize += 1
+    #puts "Added gate queue"
+  end
+end
+
+
+
+class If < Op
+  config :predicate
+  input :ifin, :doif
+  output :ifouttrue
+  output :ifoutfalse
+   
+  def initialize
+    super
+    @predicate = 'not value.nil?'
+  end
+   
+  def doif(value)   
+    s = "#{predicate}"           
+    result = instance_eval(s)
+   
+    if result == true           
+      ifouttrue.out value
+    else     
+      ifoutfalse.out value
+    end
+  end
+end
+
+
+class TextTemplate < Op
+  config :template
+  input :valuesin, :rendertemplate
+  input :templatein, :template
+  output :templateout
+
+  def initialize
+    super
+    @template = '#{value}'
+  end
+ 
+  def rendertemplate(value)       
+    s = instance_eval('"' + @template + '"')
+    templateout.out(s) 
+  end
+       
+end
+
+class Join < Op
+  input :joinin, :dojoin
+  output :joinout
+  config :joinchar
+
+  def initialize
+    super
+    @joinchar = ","
+  end
+
+  def dojoin(value)  
+    joinout.out value.to_a.join(@joinchar)
+  end
+end
+
+class Split < Op
+  input :splitin, :dosplit
+  output :splitout
+  config :splitchar
+
+  def initialize
+    super
+    @splitchar = ","
+  end
+
+  def dosplit(value)    
+    splitout.out value.to_s.split(@splitchar)
+  end
+end
+
+# swiss army knife type op
+class Mod < Op
+  input :modin, :mod
+  output :modout
+  config :code
+ 
+  def initialize
+    super
+    @code = ""
+  end
+
+  def mod(value)
+    modout.out(instance_eval(@code))
+  end
+end
+
+
+class Each < Op
+  input :eachin, :doeach
+  output :eachout
+
+  def doeach(value)
+    value.to_a.each do |item|
+      eachout.out(item)
+    end
+  end
+end
+
+
+end #module
---
+++ test/unittests.rb
@@ -0,0 +1,111 @@
+#    
+#    MetaMatter flow based scripting system
+#    Copyright (C) 2009 Dave Parfitt
+# 
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+# 
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+# 
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+
+require 'test/unit'
+require 'mm/mm.rb'
+require 'stringio'
+
+class TC_Simple < Test::Unit::TestCase
+  def setup
+  end
+
+  def teardown
+  end
+
+  def test_simple
+	s = StringIO.new()
+	net = MM::Network.new do
+  		foo = create :StartupEcho
+  		output = create :Print
+  		output.fd = s
+  		foo.echoout >> output.printin
+	end
+	net.run
+	s.rewind
+	assert(s.readline == "Foo\n","Simple test failed :-(")
+  end
+
+  def test_gate1
+	s = StringIO.new()
+	net = MM::Network.new do
+  	foo1 = create :StartupEcho
+  		foo2 = create :StartupEcho
+		gate = create :Gate
+		output = create :Print
+		join = create :Join
+
+		output.fd = s
+
+		foo1.message = "Foo1"
+		foo2.message = "Foo2"
+
+		foo1.echoout >> gate.gatein
+		foo2.echoout >> gate.gatein
+		gate.gateout >> join.joinin
+		join.joinout >> output.printin
+	end
+	net.run
+	s.rewind
+	assert(s.readline == "Foo1,Foo2\n")
+  end
+
+  def test_gate2
+	s = StringIO.new()
+	net = MM::Network.new do
+  		echo1= create :StartupEcho
+  		echo2 = create :StartupEcho
+  		echo3= create :StartupEcho
+
+		gate = create :Gate
+		output = create :Print
+		join = create :Join
+
+		output.fd = s
+
+		echo1.message = "a"
+		echo2.message = "b"
+		echo3.message = "c"
+
+		echo1.echoin.queue("a")
+		echo1.echoin.queue("a")
+		echo2.echoin.queue("b")
+		echo3.echoin.queue("c")
+		echo3.echoin.queue("c")
+		echo3.echoin.queue("c")
+
+		echo1.echoin.queue("a")
+		echo2.echoin.queue("b")
+		echo2.echoin.queue("b")
+
+		echo1.echoout >> gate.gatein
+		echo2.echoout >> gate.gatein
+		echo3.echoout >> gate.gatein
+		gate.gateout >> join.joinin
+		join.joinout >> output.printin
+	end
+	net.run
+	s.rewind
+	# the stringio should contain three lines
+	assert(s.readline == "a,b,c\n")
+	assert(s.readline == "a,b,c\n")
+	assert(s.readline == "a,b,c\n")
+  end
+  
+end
